f8b369ab489ac0cfe3f97866f491043f22ffe4b1
oRes321 <oreschaos4@gmail.com>
2024-09-25 11:55:33 +0300

    Task

---
+++ Numbers/target/debug/deps/Numbers.d
@@ -0,0 +1,16 @@
+H:\my life\Основи алгоритмізації та програмування\Rust_PROJECT\Practical_work_of_operating_systems\Numbers\target\debug\deps\Numbers.exe: src/main.rs src\Task_1.rs src\Task_2.rs src\Task_3.rs src\Task_4.rs src\Task_5.rs src\Task_6.rs src\Task_7.rs src\Task_8.rs src\Task_9.rs src\Task_10.rs src\Task_11.rs
+
+H:\my life\Основи алгоритмізації та програмування\Rust_PROJECT\Practical_work_of_operating_systems\Numbers\target\debug\deps\Numbers.d: src/main.rs src\Task_1.rs src\Task_2.rs src\Task_3.rs src\Task_4.rs src\Task_5.rs src\Task_6.rs src\Task_7.rs src\Task_8.rs src\Task_9.rs src\Task_10.rs src\Task_11.rs
+
+src/main.rs:
+src\Task_1.rs:
+src\Task_2.rs:
+src\Task_3.rs:
+src\Task_4.rs:
+src\Task_5.rs:
+src\Task_6.rs:
+src\Task_7.rs:
+src\Task_8.rs:
+src\Task_9.rs:
+src\Task_10.rs:
+src\Task_11.rs:
