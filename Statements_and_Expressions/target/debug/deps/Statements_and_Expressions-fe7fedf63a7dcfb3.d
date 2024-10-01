ace6a94b31508deb29cbc9b63fbdee171e498586
oRes321 <oreschaos4@gmail.com>
2024-10-01 08:20:00 +0300

    Task

---
+++ Statements_and_Expressions/target/debug/deps/Statements_and_Expressions-fe7fedf63a7dcfb3.d
@@ -0,0 +1,9 @@
+H:\my life\Основи алгоритмізації та програмування\Rust_PROJECT\Practical_work_of_operating_systems\Statements_and_Expressions\target\debug\deps\Statements_and_Expressions-fe7fedf63a7dcfb3.exe: src/main.rs src\task_1_1.rs src\task_1_2.rs src\task_2.rs src\task_3.rs
+
+H:\my life\Основи алгоритмізації та програмування\Rust_PROJECT\Practical_work_of_operating_systems\Statements_and_Expressions\target\debug\deps\Statements_and_Expressions-fe7fedf63a7dcfb3.d: src/main.rs src\task_1_1.rs src\task_1_2.rs src\task_2.rs src\task_3.rs
+
+src/main.rs:
+src\task_1_1.rs:
+src\task_1_2.rs:
+src\task_2.rs:
+src\task_3.rs:
