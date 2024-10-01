5930a8212129888c1bf58863f48b208416144abf
oRes321 <oreschaos4@gmail.com>
2024-10-01 07:56:51 +0300

    Task

---
+++ Statements_and_Expressions/src/task_1_1.rs
@@ -0,0 +1,14 @@
+#[test]
+
+// Make it work with two ways
+fn main() {
+    let v = {
+        let mut x = 1;
+        x += 2;
+        x
+    };
+
+    assert_eq!(v, 3);
+
+    println!("Success!");
+}
