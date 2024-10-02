6892e7d514c3829c9f9699f415dae50baa8c2fb1
oRes321 <oreschaos4@gmail.com>
2024-10-02 07:11:02 +0300

    Task

---
+++ Ownership_and_Borrowing/Reference_and_Borrowing_5_2/src/main.rs
@@ -0,0 +1,15 @@
+mod task_1;
+mod task_2;
+mod task_3;
+mod task_4;
+mod task_5;
+mod task_6;
+mod task_7;
+mod task_8;
+mod task_9;
+mod task_10;
+mod task_11;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ Ownership_and_Borrowing/Reference_and_Borrowing_5_2/target/debug/deps/Reference_and_Borrowing_5_2-359e5cf32facacee.d
@@ -0,0 +1,16 @@
+H:\Rust_PROJECT\Practical_work_of_operating_systems\Ownership_and_Borrowing\Reference_and_Borrowing_5_2\target\debug\deps\Reference_and_Borrowing_5_2-359e5cf32facacee.exe: src/main.rs src\task_1.rs src\task_2.rs src\task_3.rs src\task_4.rs src\task_5.rs src\task_6.rs src\task_7.rs src\task_8.rs src\task_9.rs src\task_10.rs src\task_11.rs
+
+H:\Rust_PROJECT\Practical_work_of_operating_systems\Ownership_and_Borrowing\Reference_and_Borrowing_5_2\target\debug\deps\Reference_and_Borrowing_5_2-359e5cf32facacee.d: src/main.rs src\task_1.rs src\task_2.rs src\task_3.rs src\task_4.rs src\task_5.rs src\task_6.rs src\task_7.rs src\task_8.rs src\task_9.rs src\task_10.rs src\task_11.rs
+
+src/main.rs:
+src\task_1.rs:
+src\task_2.rs:
+src\task_3.rs:
+src\task_4.rs:
+src\task_5.rs:
+src\task_6.rs:
+src\task_7.rs:
+src\task_8.rs:
+src\task_9.rs:
+src\task_10.rs:
+src\task_11.rs:
