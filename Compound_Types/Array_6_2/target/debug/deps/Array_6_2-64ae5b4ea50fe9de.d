22f3fb00076638ba31e333cb40ea134dda7b7c42
oRes321 <oreschaos4@gmail.com>
2024-10-03 20:27:13 +0300

    Task

---
+++ Compound_Types/Array_6_2/target/debug/deps/Array_6_2-64ae5b4ea50fe9de.d
@@ -0,0 +1,11 @@
+H:\Rust_PROJECT\Practical_work_of_operating_systems\Compound_Types\Array_6_2\target\debug\deps\Array_6_2-64ae5b4ea50fe9de.exe: src/main.rs src\task_1.rs src\task_2.rs src\task_3.rs src\task_4.rs src\task_5.rs src\task_6.rs
+
+H:\Rust_PROJECT\Practical_work_of_operating_systems\Compound_Types\Array_6_2\target\debug\deps\Array_6_2-64ae5b4ea50fe9de.d: src/main.rs src\task_1.rs src\task_2.rs src\task_3.rs src\task_4.rs src\task_5.rs src\task_6.rs
+
+src/main.rs:
+src\task_1.rs:
+src\task_2.rs:
+src\task_3.rs:
+src\task_4.rs:
+src\task_5.rs:
+src\task_6.rs:
