5ccab77661b0f2bcafcdee6b715a0ce0ef947f3c
oRes321 <oreschaos4@gmail.com>
2024-10-07 08:47:10 +0300

    Task

---
+++ Flow_Control_7/src/task_3.rs
@@ -0,0 +1,11 @@
+#[test]
+
+fn main() {
+    for n in 1..=100 { // modify this line to make the code work
+        if n == 101 {
+            panic!("NEVER LET THIS RUN")
+        }
+    }
+
+    println!("Success!");
+}
---
+++ Flow_Control_7/target/debug/deps/Flow_Control_7-7c5b9c02b9002ec3.d
@@ -0,0 +1,16 @@
+H:\Rust_PROJECT\Practical_work_of_operating_systems\Flow_Control_7\target\debug\deps\Flow_Control_7-7c5b9c02b9002ec3.exe: src/main.rs src\task_1.rs src\task_2.rs src\task_3.rs src\task_4.rs src\task_5.rs src\task_6.rs src\task_7.rs src\task_8.rs src\task_9.rs src\task_10.rs src\task_11.rs
+
+H:\Rust_PROJECT\Practical_work_of_operating_systems\Flow_Control_7\target\debug\deps\Flow_Control_7-7c5b9c02b9002ec3.d: src/main.rs src\task_1.rs src\task_2.rs src\task_3.rs src\task_4.rs src\task_5.rs src\task_6.rs src\task_7.rs src\task_8.rs src\task_9.rs src\task_10.rs src\task_11.rs
+
+src/main.rs:
+src\task_1.rs:
+src\task_2.rs:
+src\task_3.rs:
+src\task_4.rs:
+src\task_5.rs:
+src\task_6.rs:
+src\task_7.rs:
+src\task_8.rs:
+src\task_9.rs:
+src\task_10.rs:
+src\task_11.rs:
