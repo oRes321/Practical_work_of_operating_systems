544ce374f9d853f3394e16295e949b06f700b0e6
oRes321 <oreschaos4@gmail.com>
2024-10-08 20:16:52 +0300

    Task

---
+++ Pattern_Match_8/match_8_1/target/debug/deps/match_8_1-7623fe4e788d2347.d
@@ -0,0 +1,14 @@
+H:\Rust_PROJECT\Practical_work_of_operating_systems\Pattern Match_8\match_8_1\target\debug\deps\match_8_1-7623fe4e788d2347.exe: src/main.rs src\taks_1.rs src\taks_2.rs src\taks_3.rs src\taks_4.rs src\taks_5.rs src\taks_6.rs src\taks_7.rs src\taks_8.rs src\taks_9.rs
+
+H:\Rust_PROJECT\Practical_work_of_operating_systems\Pattern Match_8\match_8_1\target\debug\deps\match_8_1-7623fe4e788d2347.d: src/main.rs src\taks_1.rs src\taks_2.rs src\taks_3.rs src\taks_4.rs src\taks_5.rs src\taks_6.rs src\taks_7.rs src\taks_8.rs src\taks_9.rs
+
+src/main.rs:
+src\taks_1.rs:
+src\taks_2.rs:
+src\taks_3.rs:
+src\taks_4.rs:
+src\taks_5.rs:
+src\taks_6.rs:
+src\taks_7.rs:
+src\taks_8.rs:
+src\taks_9.rs:
