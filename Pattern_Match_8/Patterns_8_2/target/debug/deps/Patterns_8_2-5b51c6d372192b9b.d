503d9981d6d16d8e9395ad1172313605541ea3ae
oRes321 <oreschaos4@gmail.com>
2024-10-09 20:20:13 +0300

    Task

---
+++ Pattern_Match_8/Patterns_8_2/target/debug/deps/Patterns_8_2-5b51c6d372192b9b.d
@@ -0,0 +1,8 @@
+H:\Rust_PROJECT\Practical_work_of_operating_systems\Pattern_Match_8\Patterns_8_2\target\debug\deps\Patterns_8_2-5b51c6d372192b9b.exe: src/main.rs src\task_1.rs src\task_2.rs src\task_3.rs
+
+H:\Rust_PROJECT\Practical_work_of_operating_systems\Pattern_Match_8\Patterns_8_2\target\debug\deps\Patterns_8_2-5b51c6d372192b9b.d: src/main.rs src\task_1.rs src\task_2.rs src\task_3.rs
+
+src/main.rs:
+src\task_1.rs:
+src\task_2.rs:
+src\task_3.rs:
